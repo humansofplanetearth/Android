34510acc1434ada65f8064e1dc74e1b3851b9a37
japacible <japacible@gmail.com>
2014-05-29 09:54:17 -0700

    create android project and pull everything into packages

---
+++ app/src/main/java/com/hony/app/Model/Alt_sizes.java
@@ -0,0 +1,35 @@
+package com.hony.app.Model;
+
+public class Alt_sizes{
+    private Number height;
+    private String url;
+    private Number width;
+
+    public Alt_sizes() {
+
+    }
+
+    public Number getHeight(){
+    return this.height;
+    }
+
+    public void setHeight(Number height){
+    this.height = height;
+    }
+
+    public String getUrl(){
+    return this.url;
+    }
+
+    public void setUrl(String url){
+    this.url = url;
+    }
+
+    public Number getWidth(){
+    return this.width;
+    }
+
+    public void setWidth(Number width){
+    this.width = width;
+    }
+}
---
+++ app/src/main/java/com/hony/app/Model/Image.java
@@ -0,0 +1,26 @@
+package com.hony.app.Model;
+
+public class Image{
+    private Meta meta;
+    private Response response;
+
+    public Image() {
+
+    }
+
+    public Meta getMeta(){
+    return this.meta;
+    }
+
+    public void setMeta(Meta meta){
+    this.meta = meta;
+    }
+
+    public Response getResponse(){
+    return this.response;
+    }
+
+    public void setResponse(Response response){
+    this.response = response;
+    }
+}
