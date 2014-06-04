af9cb4ca1a7c765cecd8651dbcf0033d545947d3
dennisschagt <dennisschagt@gmail.com>
2014-06-04 18:02:50 +0200

    Added quick and dirty functionality for loading url's and showing an image

--- app/src/main/java/com/hony/app/Activities/MainActivity.java
+++ app/src/main/java/com/hony/app/Activities/MainActivity.java
@@ -1,6 +1,7 @@
 package com.hony.app.Activities;
 
 import android.graphics.drawable.Drawable;
+import android.os.AsyncTask;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -10,8 +11,10 @@ import com.hony.app.R;
 import com.hony.app.Utilities.PictureURLGetter;
 
 import android.content.Intent;
+import android.widget.ImageView;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.URL;
 
 
@@ -21,19 +24,40 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // FIXME: Experimental code
-        PictureURLGetter pictureURLGetter = new PictureURLGetter();
-        try {
-            URL imageURL = pictureURLGetter.next();
-            // http://stackoverflow.com/questions/8937180/how-to-show-image-in-android-using-java-code
-            // http://stackoverflow.com/questions/3375166/android-drawable-images-from-url
 
-        } catch (IOException e) {
-            // TODO: Show a message indicating that something went wrong (most probably no internet connection)
-            e.printStackTrace();
-        }
+        new DownloadImageTask().execute();
     }
 
+    // FIXME: Experimental, copied from (http://stackoverflow.com/questions/3375166/android-drawable-images-from-url)
+    Drawable drawable_from_url(URL url, String src_name) throws java.net.MalformedURLException, java.io.IOException {
+        return Drawable.createFromStream(((InputStream)url.getContent()), src_name);
+    }
+
+    private class DownloadImageTask extends AsyncTask<Void, Void, Drawable> {
+        // FIXME: Experimental code
+
+        // TODO: Do something sensible if there are multiple urls
+        protected Drawable doInBackground(Void... notUsed) {
+            PictureURLGetter pictureURLGetter = new PictureURLGetter();
+            try {
+                URL imageURL = pictureURLGetter.next();
+                Drawable drawable = drawable_from_url(imageURL, "");
+                return drawable;
+            } catch (IOException e) {
+                // TODO: Show a message indicating that something went wrong (most probably no internet connection)
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        protected void onPostExecute(Drawable drawable) {
+            if (drawable == null) {
+                // TODO: Show an error message somewhere
+            }
+            ImageView imgView=(ImageView) findViewById(R.id.imageView);
+            imgView.setImageDrawable(drawable);
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
